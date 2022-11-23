2e9fc0645f3cb9284195452a3e72823520ff0a95
Fabian Mendez <ofmendez@gmail.com>
2022-11-22 21:12:32 -0500

    Nuevos valores de probabilidad /puntos

--- js/own/views.js
+++ js/own/views.js
@@ -21,5 +21,6 @@
     }
      export const viewAv = ['Login','TerminosYCondiciones','Felicitaciones','SigueIntentando']
      export const viewDat = {'Rango1': 3, 'Rango2' : 5 , 'Rango3': 6}
-     export const viewDat2 = {'Rango1': 160, 'Rango2' : 240 , 'Rango3': 240}
+    //  export const viewDat2 = {'Rango1': 160, 'Rango2' : 240 , 'Rango3': 240}
+     export const viewDat2 = {'Rango1': 180, 'Rango2' : 270 , 'Rango3': 270}
      export const viewDat3 = {'Rango1': 20, 'Rango2' : 30 , 'Rango3': 50}
